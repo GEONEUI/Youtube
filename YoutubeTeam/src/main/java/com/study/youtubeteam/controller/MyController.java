69e8522898046c3d8640bfeffeae486b9851ede9
dbrjsdml <dbrjsdml@naver.com>
2023-02-07 15:19:58 +0900

    세팅완료

---
+++ YoutubeTeam/src/main/java/com/study/youtubeteam/controller/MyController.java
@@ -0,0 +1,32 @@
+package com.study.youtubeteam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	//건의
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	//예준
+	@RequestMapping("/play")
+	public String play() {
+		return "play";
+	}
+	
+	//준호
+	@RequestMapping("/channel")
+	public String channel() {
+		return "channel";
+	}
+	
+	//유진
+	@RequestMapping("/mypage")
+	public String mypage() {
+		return "mypage";
+	}
+}
