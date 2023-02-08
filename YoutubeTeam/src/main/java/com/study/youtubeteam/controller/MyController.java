e7bb017a5ab6391c108acff7c505665199ec10c9
dbrjsdml <dbrjsdml@naver.com>
2023-02-08 16:59:13 +0900

    무한스크롤 구현

--- YoutubeTeam/src/main/java/com/study/youtubeteam/controller/MyController.java
+++ YoutubeTeam/src/main/java/com/study/youtubeteam/controller/MyController.java
@@ -1,14 +1,31 @@
 package com.study.youtubeteam.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.study.youtubeteam.emtity.youtubeList;
+import com.study.youtubeteam.mapper.YoutubeListMapper;
+
+
+
+
 
 @Controller
 public class MyController {
 	
+	@Autowired
+	YoutubeListMapper mapper;
+
 	//건의
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
+		List<youtubeList> list = mapper.selectAll();
+		System.out.println(list);
+		model.addAttribute("list", list);
 		return "index";
 	}
 	
