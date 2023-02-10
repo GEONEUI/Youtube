8a9f6fafdbb9eb6e1327a5d24078df9762e8aa61
thxb5 <117423418+thxb5@users.noreply.github.com>
2023-02-10 20:34:29 +0900

    mypg,4개 목록

--- YoutubeTeam/src/main/java/com/study/youtubeteam/controller/MyController.java
+++ YoutubeTeam/src/main/java/com/study/youtubeteam/controller/MyController.java
@@ -5,46 +5,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
-	
-	//건의
+
+	// 건의
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
-	
-	//예준
+
+	// 예준
 	@RequestMapping("/play")
 	public String play() {
 		return "play";
 	}
-	
-	//준호
+
+	// 준호
 	@RequestMapping("/channel")
 	public String channel() {
 		return "channel";
 	}
-	
-	//유진
+
+	// 유진
 	@RequestMapping("/mypage")
 	public String mypage() {
 		return "mypage";
 	}
-	
-	//유진-보관함
+
+	// 유진-보관함
 	@RequestMapping("/videos")
 	public String videos() {
 		return "videos";
 	}
-	
-	//유진-시청기록
-		@RequestMapping("/watchtime")
-		public String watchtime() {
-			return "watchtime";
-		}
-		
-	//유진-구독
-		@RequestMapping("/subscribe")
-		public String subscribe() {
-			return "subscribe";
-		}
+
+	// 유진-시청기록
+	@RequestMapping("/watchtime")
+	public String watchtime() {
+		return "watchtime";
+	}
+
+	// 유진-구독
+	@RequestMapping("/subscribe")
+	public String subscribe() {
+		return "subscribe";
+	}
+
+	// 유진-댓글
+	@RequestMapping("/comment")
+	public String comment() {
+		return "comment";
+	}
 }
